cbdf4e1637d0fdfb846917d909bd87ea93dee72e
Samkayzeee <lasisiabdulsamad9@gmail.com>
2023-05-13 17:41:33 +0100

    worked on home page searching

--- src/components/main_content/Content.jsx
+++ src/components/main_content/Content.jsx
@@ -16,12 +16,18 @@ const Content = () => {
 
                 <div className="search">
                 <img src={profile_pic} alt="profile_pic" />
-                
+
                     <div className="main-search">
-                        <input type="text" placeholder='What is Happening?'/>
+                        <input type="text" placeholder='What is Happening?!'/>
                         <div className="links">
                             <div className="main-links">
-
+                                <i className="fa-regular fa-image"></i>
+                                {/* <i className="fa-sharp fa-regular fa-gif"></i> */}
+                                <span>2</span>
+                                <span>3</span>
+                                <span>4</span>
+                                <span>5</span>
+                                <span>6</span>
                             </div>
                             <button>Tweet</button>
                         </div>
